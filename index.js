be3a89ddf654ccf415d143549f0cac5130803cb7
seyun0714 <seun0714@naver.com>
2025-10-16 12:48:05 +0900

    feat: 카드 컴포넌트 관련 코드 삭제 및 JSON파일에서 초기 카드 리스트 로드 설정

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 import { $ } from "/utils/common.js";
+import { loadDummyData } from "/utils/load.js";
+import { createCard, createCards } from "/components/card/card.js";
 
 let [level, setLevel] = ["전체", (input) => (level = input)];
 let [sort, setSort] = ["인기순", (input) => (sort = input)];
 
+// 난이도 필터 모달 토글 함수
 function toggleLevelFilter() {
   const $levelOptions = $(".level-options");
   $levelOptions.classList.toggle("visible");
-  $(".btn-level svg").classList.toggle("rotate");
+  $(".btn-level i").classList.toggle("rotate");
 }
 
+// 정렬 필터 모달 토글 함수
 function toggleSortFilter() {
   const $levelOptions = $(".sort-options");
   $levelOptions.classList.toggle("visible");
 }
 
+// 강의 수정/삭제 메뉴 모달 오픈 함수
 function openCardMenu(e) {
   document.querySelectorAll(".card-menu-options").forEach((node) => {
     node.classList.remove("visible");
@@ -23,19 +28,19 @@ function openCardMenu(e) {
   $cardMenuOptions.classList.add("visible");
 }
 
-// 난이도 필터 옵션 온오프
+// 난이도 필터 버튼 온오프 클릭 이벤트 등록
 $(".btn-level").addEventListener("click", (e) => {
   e.preventDefault();
   toggleLevelFilter();
 });
 
-// 정렬 필터 옵션 온오프
+// 정렬 필터 버튼 온오프 클릭 이벤트 등록
 $(".btn-sort").addEventListener("click", (e) => {
   e.preventDefault();
   toggleSortFilter();
 });
 
-// 난이도 필터 옵션 선택
+// 난이도 필터 옵션 선택 클릭 이벤트 등록
 document.querySelectorAll(".level-label").forEach(($levelLabel) => {
   $levelLabel.addEventListener("click", (e) => {
     e.preventDefault();
@@ -50,7 +55,7 @@ document.querySelectorAll(".level-label").forEach(($levelLabel) => {
   });
 });
 
-// 정렬 필터 옵션 선택
+// 정렬 필터 옵션 선택 클릭 이벤트 등록
 document.querySelectorAll(".sort-label").forEach(($sortLabel) => {
   $sortLabel.addEventListener("click", (e) => {
     e.preventDefault();
@@ -65,7 +70,7 @@ document.querySelectorAll(".sort-label").forEach(($sortLabel) => {
   });
 });
 
-// 강의 수정/삭제 메뉴 모달 온오프
+// 강의 수정/삭제 메뉴 모달 온오프 클릭 이벤트 등록
 document.querySelectorAll(".card-menu").forEach((cardMenu) => {
   cardMenu.addEventListener("click", (e) => {
     e.preventDefault();
@@ -73,13 +78,13 @@ document.querySelectorAll(".card-menu").forEach((cardMenu) => {
   });
 });
 
-// 모달 바깥 클릭 시 모달 오프
+// 모달 바깥 클릭 시 모달 오프 클릭 이벤트 등록
 document.addEventListener("click", (e) => {
   // 난이도 필터 닫기
   const isLevelFilter = e.target.closest(".level");
   if (!isLevelFilter) {
     $(".level-options").classList.remove("visible");
-    $(".btn-level svg").classList.remove("rotate");
+    $(".btn-level i").classList.remove("rotate");
   }
   // 정렬 필터 닫기
   const isSortFilter = e.target.closest(".sort");
@@ -94,4 +99,40 @@ document.addEventListener("click", (e) => {
       node.classList.remove("visible");
     });
   }
+});
+
+// 초기 데이터 로드
+document.addEventListener("DOMContentLoaded", async () => {
+  const lectures =
+    JSON.parse(localStorage.getItem("data")) || (await loadDummyData());
+
+  const cardDatas = lectures.map((lecture) => {
+    return {
+      lectureId: lecture.lectureId,
+      title: lecture.title,
+      category: lecture.category,
+      authorId: lecture.authorId,
+      thumbnail: lecture.thumbnail,
+      onCardClick: () => {
+        window.location.href = `/pages/detail/detail.html?id=${lecture.lectureId}`;
+      },
+      onCardMenuClick: (e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        openCardMenu(e);
+      },
+      onModifyClick: (e) => {
+        console.log("수정 클릭");
+      },
+      onDeleteClick: (e) => {
+        console.log("삭제 클릭");
+      },
+    };
+  });
+
+  const cardList = await createCards(cardDatas);
+
+  cardList.forEach((card) => {
+    $(".main-list-grid").appendChild(card);
+  });
 });
